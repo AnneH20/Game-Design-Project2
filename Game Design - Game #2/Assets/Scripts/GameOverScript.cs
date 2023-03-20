ef46a93aaca338b2803f69890c2f190d51e3e578
clj500 <clj500@msstate.edu>
2023-03-20 03:27:34 -0500

    Assorted changes
    Fixed control button on homescreen
    Added music to control screen
    Progress on the gameover screen

--- Game Design - Game #2/Assets/Scripts/GameOverScript.cs
+++ Game Design - Game #2/Assets/Scripts/GameOverScript.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameOverScript : MonoBehaviour
 {
@@ -21,5 +22,15 @@ public class GameOverScript : MonoBehaviour
     public void gameOver()
     {
         gameOverUI.SetActive(true);
+    }
+
+    public void rematch()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void mainMenu()
+    {
+        SceneManager.LoadScene("Homescreen");
     }
 }
